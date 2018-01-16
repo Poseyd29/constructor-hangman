9fc1aca74300b9533b220f8f0f26ccafeeb1babc
Newman Posey <newman@newmans-mbp.attlocal.net>
2018-01-16 13:36:14 -0500

    first commit

---
+++ game.js
@@ -0,0 +1,4 @@
+exports.wordBank = {
+	wordList: ['TIMBERWOLVES','JAGUARS','CAVELIERS','CLIPPERS','PIRATES','PANTHERS','ASTROS','ROCKETS','CARDINALS','PACERS']
+	hangman: ["\n\n\n\n\n\n\n\n\n\n\n-------------", "\n\n\n\n"]
+}
---
+++ hangman.js
@@ -0,0 +1,23 @@
+var inquirer = require('inquirer');
+
+var Word = require('constructor-hangman/word.js');
+
+var Hangman = require('constructor-hangman/hangman.js');
+
+var gameDisplay = 
+
+
+// require events
+
+var hangman = {
+
+	wordList:
+	guessesRemaining: 10,
+	letterGuessed: [],
+	currentWord: null,
+
+	startGame: function() {
+		var that = this;
+	}
+}
+
